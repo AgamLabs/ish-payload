4eb4d601731c18abfc9d1584066d3430232b910d
cvalingam <cvalingam-r@hotmail.com>
2025-04-14 02:11:30 +0530

    ci: fixed vercel blob storage

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,41 +1,46 @@
-import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from "payload";
 
 import {
   FixedToolbarFeature,
   InlineToolbarFeature,
   lexicalEditor,
-} from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { fileURLToPath } from 'url'
+} from "@payloadcms/richtext-lexical";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
-const VERCEL_BLOB_URL = process.env.VERCEL_BLOB_URL;
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export const Media: CollectionConfig = {
-  slug: 'media',
+  slug: "media",
   access: {
     read: () => true,
   },
   fields: [
     {
-      name: 'alt',
-      type: 'text',
+      name: "alt",
+      type: "text",
       required: true,
     },
     {
-      name: 'caption',
-      type: 'richText',
+      name: "caption",
+      type: "richText",
       editor: lexicalEditor({
         features: ({ rootFeatures }) => {
-          return [...rootFeatures, FixedToolbarFeature(), InlineToolbarFeature()]
+          return [
+            ...rootFeatures,
+            FixedToolbarFeature(),
+            InlineToolbarFeature(),
+          ];
         },
       }),
     },
   ],
   upload: {
-    staticDir: process.env.NODE_ENV === 'production'
-      ? VERCEL_BLOB_URL // In production, use Vercel Blob storage
-      : path.resolve(dirname, '../../public/media'), // In development, use local storage
+    staticDir:
+      process.env.NODE_ENV === "production"
+        ? undefined // In production, use Vercel Blob storage
+        : path.resolve(dirname, "../../public/media"), // In development, use local storage
+    disableLocalStorage: true,
   },
-}
+};
